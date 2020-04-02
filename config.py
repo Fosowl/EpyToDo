516b7f8696347e382773d68d3ae190507b613436
mathias <mathias.ressort@epitech.eu>
2020-04-02 02:33:38 +0200

    [Add] Hello world

--- config.py
+++ config.py
@@ -5,17 +5,3 @@
 ## config
 ##
 
-## Flash variables
-DEBUG	      	= True
-TESTING 	    = True
-
-## Database connection variables
-DATABASE_NAME 	= "epytodo"
-DATABASE_HOST 	= "127.0.0.1"
-DATABASE_SOCK 	= None
-DATABASE_USER 	= "root"
-DATABASE_PASS 	= ""
-
-## Hashing variables
-## WARN: If this variables is edited, all password may be incorrect
-PASSWORD_SALT   = "oui"
